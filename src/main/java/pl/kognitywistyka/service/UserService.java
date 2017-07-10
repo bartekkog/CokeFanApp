7f3dea83fcab5cea758f1243ecbec25e1e465a03
Anna Baljon <anna.baljon@gmail.com>
2017-07-10 17:03:19 +0200

    adding/deleting users

--- src/main/java/pl/kognitywistyka/service/UserService.java
+++ src/main/java/pl/kognitywistyka/service/UserService.java
@@ -68,7 +68,48 @@ public class UserService {
             session.close(); //NO W KONCU :D
         }
         return finalList;
+    }
+
+    //Adding users to DB
+
+    public boolean add(user user){
+        user.setPassword(AuthenticationService.sha256(user.getPassword()));
+        Session session = HibernateUtils.getSessionFactory().openSession();
+        Transaction tr = null;
+
+        try{
+            tr = session.beginTransaction();
+            session.save(user);
+            tr.commit();
+        } catch (Exception e){
+            if (tr!=null) tr.rollback();
+            e.printStackTrace();
+            return false;
+        } finally {
+            session.close();
+        }
+    return true;}
 
 
+//Deleting users from DB
+
+public boolean delete(user user){
+    Session session = HibernateUtils.getSessionFactory().openSession();
+    Transaction tr = null;
+    try {
+        tr = session.beginTransaction();
+        String uID = user.getId();
+        session.load(user.class, uID);
+        session.delete(user);
+        session.getTransaction().commit();
+
+    } catch (Exception e){
+        if(tr!=null) tr.rollback();
+        e.printStackTrace();
+        return false;
+    } finally {
+        session.close();
+    }
+    return true;
     }
 }
